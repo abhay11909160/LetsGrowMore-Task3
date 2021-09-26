d3bb25064c96ece04eeda7668a2a6e869ecbb02f
abhay11909160 <63112112+abhay11909160@users.noreply.github.com>
2021-09-26 14:20:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SSRMS</title>
+    <title>SRMS</title>
     <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
