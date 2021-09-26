41ecaca17cec5ea1ab713b55ba4ca6b39fe5ec62
abhay11909160 <63112112+abhay11909160@users.noreply.github.com>
2021-09-26 13:55:33 +0530

    Delete studentresult directory

--- studentresult/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$conn = mysqli_connect($servername, $username, $password);
-	$db = mysqli_select_db($conn,'simplestresult');
-?>
